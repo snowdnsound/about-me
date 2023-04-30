8a5ad29631b63a2856a6b0d9351c03aae95c5100
Justin Benjamin <justinjbenjamin@gmail.com>
2023-04-29 21:46:10 -0700

    fixed scoring

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let userName = prompt('What is your name?');
 let score = 0;
 let theirName = 'Welcome to my site ' + userName + ' but before we go there, how about we play a guessing game!';
@@ -8,81 +8,98 @@ alert(theirName);
 
 
 let catQuestion = prompt('Do I have cats? Yes/No?');
+console.log('Do I have cats?');
 let correctCat = 'That is correct!';
 let wrongCat = 'Nope, I have two cats!';
 if (catQuestion.toLowerCase() === 'yes' || catQuestion.toLowerCase() === 'y') {
   console.log(catQuestion);
   alert(correctCat);
   score++;
+  console.log(score);
 } else if (catQuestion.toLowerCase() === 'no' || catQuestion.toLowerCase() === 'n') {
   console.log(catQuestion);
   alert(wrongCat);
   score--;
+  console.log(score);
 }
 
 
 let gamerQuestion = prompt('Am I a gamer? Yes/No?');
+console.log('Am I a gamer?');
 let correctGamer = 'That is correct!';
 let wrongGamer = 'Nope, tabletop, video games, I play them all!';
 if (gamerQuestion.toLowerCase() === 'yes' || gamerQuestion.toLowerCase() === 'y') {
   console.log(gamerQuestion);
   alert(correctGamer);
   score++;
+  console.log(score);
 } else if (gamerQuestion.toLowerCase() === 'no' || gamerQuestion.toLowerCase() === 'n') {
   console.log(gamerQuestion);
   alert(wrongGamer);
   score--;
+  console.log(score);
 }
 
 
 let juggleQuestion = prompt('Can I juggle? Yes/No?');
-let correctJuggle = 'No, I can\'t do everything sheesh!';
-let wrongJuggle = 'Correct, no knife tossing for me.';
-if (juggleQuestion.toLowerCase() === 'yes' || juggleQuestion.toLowerCase() === 'y') {
+console.log('Can I juggle?');
+let correctJuggle = 'Correct, no knife tossing for me.';
+let wrongJuggle = 'No, I can\'t do everything sheesh!';
+if (juggleQuestion.toLowerCase() === 'no' || juggleQuestion.toLowerCase() === 'n') {
   console.log(juggleQuestion);
   alert(correctJuggle);
   score++;
-} else if (juggleQuestion.toLowerCase() === 'no' || juggleQuestion.toLowerCase() === 'n') {
+  console.log(score);
+} else if (juggleQuestion.toLowerCase() === 'yes' || juggleQuestion.toLowerCase() === 'y') {
   console.log(juggleQuestion);
   alert(wrongJuggle);
   score--;
+  console.log(score);
 }
 
 
 let musicQuestion = prompt('Do I like to make music? Yes/No?');
+console.log('Do I like to make music?');
 let correctMusic = 'Yes, mostly electronic/ambient music';
 let wrongMusic = 'Nope. I do actually.';
 if (musicQuestion.toLowerCase() === 'yes' || musicQuestion.toLowerCase() === 'y') {
   console.log(musicQuestion);
   alert(correctMusic);
   score++;
+  console.log(score);
 } else if (musicQuestion.toLowerCase() === 'no' || musicQuestion.toLowerCase() === 'n') {
   console.log(musicQuestion);
   alert(wrongMusic);
   score--;
+  console.log(score);
 }
 
 
 let cookQuestion = prompt('Do I like to cook? Yes/No?');
-let correctCook = 'Nope. But I do try!';
-let wrongCook = 'Correct, maybe my lazy developer mindset is affecting other areas...';
-if (cookQuestion.toLowerCase() === 'yes' || cookQuestion.toLowerCase() === 'y') {
+console.log('Do I like to cook?');
+let correctCook = 'Correct, maybe my lazy developer mindset is affecting other areas...';
+let wrongCook = 'Nope. But I do try!';
+if (cookQuestion.toLowerCase() === 'no' || cookQuestion.toLowerCase() === 'n') {
   console.log(cookQuestion);
   alert(correctCook);
   score++;
-} else if (cookQuestion.toLowerCase() === 'no' || cookQuestion.toLowerCase() === 'n') {
+  console.log(score);
+} else if (cookQuestion.toLowerCase() === 'yes' || cookQuestion.toLowerCase() === 'y') {
   console.log(cookQuestion);
   alert(wrongCook);
   score--;
+  console.log(score);
 }
 
 
 let correctAnswer = 6;
 let attemptsRemaining = 4;
 let guessedCorrectly = false;
+console.log('Guess a number between 1 and 10?');
 
 while (attemptsRemaining > 0 && !guessedCorrectly) {
   let guess = prompt('Guess a number between 1 and 10?');
+  console.log(guess);
   let guessNumber = Number(guess);
 
   if (guessNumber === correctAnswer) {
@@ -90,6 +107,7 @@ while (attemptsRemaining > 0 && !guessedCorrectly) {
     guessedCorrectly = true;
     alert('Congratulations! You guessed the correct number.');
     score++;
+    console.log(score);
   } else if (guessNumber > correctAnswer) {
     // the guess was too high
     attemptsRemaining--;
@@ -104,15 +122,18 @@ while (attemptsRemaining > 0 && !guessedCorrectly) {
 if (!guessedCorrectly) {
   alert('Sorry, you did not guess the correct number. The correct answer was ' + correctAnswer + '.');
   score--;
+  console.log(score);
 }
 
+
 let correctColorAnswer = ['purple', 'green', 'blue'];
+console.log('Guess one of my favorite colors?');
 let colorAttemptsRemaining = 6;
 let guessedColorCorrectly = false;
 
 while (colorAttemptsRemaining > 0 && !guessedColorCorrectly) {
   let guessColor = prompt('Guess one of my favorite colors? Red, blue, green, yellow, purple, pink, white or black?');
-
+  console.log(guessColor);
   let isCorrect = false;
   for (let i = 0; i < correctColorAnswer.length; i++) {
     if (guessColor.toLowerCase() === correctColorAnswer[i]) {
@@ -126,6 +147,7 @@ while (colorAttemptsRemaining > 0 && !guessedColorCorrectly) {
     guessedColorCorrectly = true;
     alert('Congratulations! You guessed a correct color!');
     score++;
+    console.log(score);
   } else {
     // the guess was not right
     colorAttemptsRemaining--;
@@ -136,11 +158,12 @@ while (colorAttemptsRemaining > 0 && !guessedColorCorrectly) {
 if (!guessedColorCorrectly) {
   alert('Sorry, you did not guess the correct color. The correct answer was ' + correctColorAnswer + '.');
   score--;
+  console.log(score);
 }
 
 
 //final message to named user at end
-console.log('YOUR SCORE IS: ', score);
+console.log('YOUR SCORE IS: ' + score);
 let theMessage = 'Thanks for playing ' + userName + ' your score is ' + score + '.' + ' Enough of that, let\'s go to my page!';
 alert(theMessage);
 
